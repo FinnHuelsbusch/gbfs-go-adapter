e59c96ac61df6953c8241cdaab0689b0a92f21ed
FinnHuelsbusch <f.huelsbusch@outlook.de>
2026-07-06 18:16:23 +0200

    fix tests

--- models/v3.1-RC3/gbfs_versions/gbfs_versions.go
+++ models/v3.1-RC3/gbfs_versions/gbfs_versions.go
@@ -1,11 +1,11 @@
 package gbfs_versions
 
 import (
-	versions_v31_rc2 "github.com/phd-kerger/gbfs-go-adapter/models/v3.1-RC2/gbfs_versions"
+	versions_v30 "github.com/phd-kerger/gbfs-go-adapter/models/v3.0/gbfs_versions"
 )
 
 // Lists all feed endpoints published according to version sof the GBFS documentation.
 // (added in v1.1)
 type GbfsVersions struct {
-	versions_v31_rc2.GbfsVersions
+	versions_v30.GbfsVersions
 }
